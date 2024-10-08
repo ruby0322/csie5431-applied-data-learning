f12155ff47702b982b2fc830e3e97eabb0136cad
Ruby <ruby0322@ntu.im>
2024-10-08 14:35:21 +0800

    fix: minor error

--- hw1/span_extraction_train.py
+++ hw1/span_extraction_train.py
@@ -474,6 +474,7 @@ def main():
         # Some of the questions have lots of whitespace on the left, which is not useful and will make the
         # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
         # left whitespace
+        
         examples[question_column_name] = [q.lstrip() for q in examples[question_column_name]]
 
         # Tokenize our examples with truncation and maybe padding, but keep the overflows using a stride. This results
