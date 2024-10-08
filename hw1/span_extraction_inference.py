644c892923a03ac142fdaf11570b3399141b5927
Ruby <ruby0322@ntu.im>
2024-10-08 23:29:55 +0800

    fix: span extraction inference data transform

--- hw1/span_extraction_inference.py
+++ hw1/span_extraction_inference.py
@@ -136,20 +136,127 @@ def main():
     extension = args.test_file.split(".")[-1]
     test_dataset = load_dataset(extension, data_files={"test": args.test_file})["test"]
 
-    # Make predictions
-    predictions = make_predictions(
-        model=model,
-        tokenizer=tokenizer,
-        test_dataset=test_dataset,
-        batch_size=args.batch_size,
-        max_seq_length=min(args.max_seq_length, tokenizer.model_max_length)
+    pad_on_right = tokenizer.padding_side == "right"
+    max_seq_length = min(args.max_seq_length, tokenizer.model_max_length)
+
+    column_names = test_dataset["train"].column_names
+
+    question_column_name = "question" if "question" in column_names else column_names[0]
+    context_column_name = "context" if "context" in column_names else column_names[1]
+    answer_column_name = "answers" if "answers" in column_names else column_names[2]
+
+     # Validation preprocessing
+    def prepare_validation_features(examples):
+        # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+        # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+        # left whitespace
+        examples[question_column_name] = [q.lstrip() for q in examples[question_column_name]]
+
+        # Tokenize our examples with truncation and maybe padding, but keep the overflows using a stride. This results
+        # in one example possible giving several features when a context is long, each of those features having a
+        # context that overlaps a bit the context of the previous feature.
+        tokenized_examples = tokenizer(
+            examples[question_column_name if pad_on_right else context_column_name],
+            examples[context_column_name if pad_on_right else question_column_name],
+            truncation="only_second" if pad_on_right else "only_first",
+            max_length=max_seq_length,
+            stride=args.doc_stride,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length" if args.pad_to_max_length else False,
+        )
+
+        # Since one example might give us several features if it has a long context, we need a map from a feature to
+        # its corresponding example. This key gives us just that.
+        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+
+        # For evaluation, we will need to convert our predictions to substrings of the context, so we keep the
+        # corresponding example_id and we will store the offset mappings.
+        tokenized_examples["example_id"] = []
+
+        for i in range(len(tokenized_examples["input_ids"])):
+            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
+            sequence_ids = tokenized_examples.sequence_ids(i)
+            context_index = 1 if pad_on_right else 0
+
+            # One example can give several spans, this is the index of the example containing this span of text.
+            sample_index = sample_mapping[i]
+            tokenized_examples["example_id"].append(examples["id"][sample_index])
+
+            # Set to None the offset_mapping that are not part of the context so it's easy to determine if a token
+            # position is part of the context or not.
+            tokenized_examples["offset_mapping"][i] = [
+                (o if sequence_ids[k] == context_index else None)
+                for k, o in enumerate(tokenized_examples["offset_mapping"][i])
+            ]
+
+        return tokenized_examples
+
+    test_dataset = test_dataset.map(
+        prepare_validation_features,
+        batched=True,
+        remove_columns=column_names,
     )
 
+    data_collator = default_data_collator
+    test_dataset_for_model = test_dataset.remove_columns(["example_id", "offset_mapping"])
+    test_dataloader = DataLoader(
+        test_dataset_for_model, collate_fn=data_collator, batch_size=args.per_device_test_batch_size
+    )
+
+    def create_and_fill_np_array(start_or_end_logits, dataset, max_len):
+        step = 0
+        # create a numpy array and fill it with -100.
+        logits_concat = np.full((len(dataset), max_len), -100, dtype=np.float64)
+        # Now since we have create an array now we will populate it with the outputs gathered using accelerator.gather_for_metrics
+        for i, output_logit in enumerate(start_or_end_logits):  # populate columns
+            # We have to fill it such that we have to take the whole tensor and replace it on the newly created array
+            # And after every iteration we have to change the step
+
+            batch_size = output_logit.shape[0]
+            cols = output_logit.shape[1]
+
+            if step + batch_size < len(dataset):
+                logits_concat[step : step + batch_size, :cols] = output_logit
+            else:
+                logits_concat[step:, :cols] = output_logit[: len(dataset) - step]
+
+            step += batch_size
+
+        return logits_concat
+
+
+    all_start_logits = []
+    all_end_logits = []
+
+    model.eval()
+
+    for step, batch in enumerate(predict_dataloader):
+        with torch.no_grad():
+            outputs = model(**batch)
+            start_logits = outputs.start_logits
+            end_logits = outputs.end_logits
+
+            all_start_logits.append(start_logits)
+            all_end_logits.append(end_logits)
+
+    print(all_start_logits)
+    print(all_end_logits)
+
+    # # Make predictions
+    # predictions = make_predictions(
+    #     model=model,
+    #     tokenizer=tokenizer,
+    #     test_dataset=test_dataset,
+    #     batch_size=args.batch_size,
+    #     max_seq_length=min(args.max_seq_length, tokenizer.model_max_length)
+    # )
+
     # Save predictions
-    with open(args.output_file, "w") as f:
-        json.dump(predictions, f)
+    # with open(args.output_file, "w") as f:
+    #     json.dump(predictions, f)
     
-    print(f"Predictions saved to {args.output_file}")
+    # print(f"Predictions saved to {args.output_file}")
 
 if __name__ == "__main__":
     main()
