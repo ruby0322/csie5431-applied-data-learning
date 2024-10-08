b03a8ed6754740fab0cbfafebec715b06f1bb57a
Ruby <ruby0322@ntu.im>
2024-10-08 17:11:07 +0800

    fix: ValueError

--- hw1/span_extraction_train.py
+++ hw1/span_extraction_train.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python
-# coding=utf-8
-# Copyright 2021 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-Fine-tuning a 🤗 Transformers model for question answering using 🤗 Accelerate.
-"""
-# You can also adapt this script on your own question answering task. Pointers for this are left as comments.
 
 import argparse
 import json
@@ -722,7 +703,7 @@ def main():
         else:
             formatted_predictions = [{"id": k, "prediction_text": v} for k, v in predictions.items()]
 
-        references = [{"id": ex["id"], "answers": ex[answer_column_name]} for ex in examples]
+        references = [{"id": ex["id"], "answers": [{ "text": ex[answer_column_name]['text'], "answer_start": ex[answer_column_name]['answer_start']}]} for ex in examples]
         return EvalPrediction(predictions=formatted_predictions, label_ids=references)
 
     metric = evaluate.load("squad_v2" if args.version_2_with_negative else "squad")
