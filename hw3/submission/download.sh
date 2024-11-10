599e64e5a7f3d1d0a46593dc62937b9926c1f3eb
Ruby <ruby0322@ntu.im>
2024-11-10 18:45:21 +0800

    feat: add submission scripts

--- hw3/submission/download.sh
+++ hw3/submission/download.sh
@@ -1,3 +1,3 @@
 gdown 1v55KH4lmNZqb-Hl2vQTdGq5o6AsvrjKS
 unzip no-preprocess-zero-shot-15.13.zip
-mv ./output/no-preprocess-zero-shot-15.13 ./adapter_checkpoint
+mv ./no-preprocess-zero-shot-15.13 ./adapter_checkpoint
